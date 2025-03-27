6cd0568ac0d59d03f405fcae40d42cc5de42c8e9
BagasSatriaYn <duhjvdedhvhjufahfy@gmail.com>
2025-03-27 20:19:12 +0700

    Praktikum 1

--- Week 6 New/app/Http/Controllers/UserController.php
+++ Week 6 New/app/Http/Controllers/UserController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\UserModel;
 use App\Models\LevelModel;
 use Yajra\DataTables\Facades\DataTables;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
@@ -136,7 +137,12 @@ class UserController extends Controller
  
          return view('user.edit', ['breadcrumb' => $breadcrumb, 'page' => $page, 'user' => $user, 'level' => $level, 'activeMenu' => $activeMenu]);
      }
- 
+     public function create_ajax() {
+        $level = LevelModel::select('level_id', 'level_name')->get();
+
+        return view('user.create_ajax')
+            -> with('level', $level);
+    }
      // Menyimpan perubahan data user
      public function update(Request $request, string $id)
      {
@@ -158,6 +164,38 @@ class UserController extends Controller
  
          return redirect('/user')->with('success', 'Data user berhasil diubah');
      }
+
+     public function store_ajax(Request $request)
+     {
+         // cek apakah request berupa ajax
+         if ($request->ajax() || $request->wantsJson()) {
+             $rules = [
+                 'level_id' => 'required|integer',
+                 'username' => 'required|string|min:3|unique:m_user,username',
+                 'nama'     => 'required|string|max:100',
+                 'password' => 'required|min:6',
+             ];
+ 
+             // use Illuminate\Support\Facades\Validator;
+             $validator = Validator::make($request->all(), $rules);
+ 
+             if ($validator->fails()) {
+                 return response()->json([
+                     'status' => false, // response status, false: error/gagal, true: berhasil
+                     'message' => 'Validasi Gagal',
+                     'msgField' => $validator->errors(), // pesan error validasi
+                 ]);
+             }
+ 
+             UserModel::create($request->all());
+             return response()->json([
+                 'status' => true,
+                 'message' => 'Data user berhasil disimpan',
+             ]);
+         }
+         return redirect('/');
+     }
+
      public function destroy(string $id)
      {
          $check = UserModel::find($id);
