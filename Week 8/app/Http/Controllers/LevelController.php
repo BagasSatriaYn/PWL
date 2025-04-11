f8d248a87faa35c338b4823afb69cfcab7efcd88
BagasSatriaYn <duhjvdedhvhjufahfy@gmail.com>
2025-04-11 21:22:34 +0700

    Tugas 3 (Export PDF)

--- Week 8/app/Http/Controllers/LevelController.php
+++ Week 8/app/Http/Controllers/LevelController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\LevelModel;
+use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Http\Request;
 use Yajra\DataTables\Facades\DataTables;
 use Illuminate\Support\Facades\Validator;
@@ -298,4 +299,19 @@ class LevelController extends Controller
         $writer->save('php://output');
         exit;
     }
+    public function export_pdf()
+    {
+        $level = LevelModel::select('level_kode', 'level_name')
+                                ->orderBy('level_id')
+                                ->orderBy('level_kode')
+                                ->get();
+    
+        // use Barryvdh\DomPDF\Facade\Pdf;
+        $pdf = Pdf::loadView('level.export_pdf', ['level' => $level]);
+        $pdf->setPaper('a4', 'portrait'); // set ukuran kertas dan orientasi
+        $pdf->setOption("isRemoteEnabled", true); // set true jika ada gambar dari url
+        $pdf->render();
+    
+        return $pdf->stream('Data Level '.date('Y-m-d H:i:s').'.pdf');
+    }       
 }
--- Week 8/routes/web.php
+++ Week 8/routes/web.php
@@ -53,6 +53,7 @@
     Route::get('/import', [UserController::class, 'import']);
     Route::post('/import_ajax', [UserController::class, 'import_ajax']);
     Route::get('/export_excel', [UserController::class, 'export_excel']); // export excel
+    Route::get('/export_pdf', [UserController::class, 'export_pdf']); // export pdf
 });
 
  Route::middleware(['authorize:ADM,MNG,STF'])->prefix('level')->group(function () {
@@ -73,6 +74,7 @@
     Route::get('/import', [LevelController::class, 'import']);
     Route::post('/import_ajax', [LevelController::class, 'import_ajax']);
     Route::get('/export_excel', [LevelController::class, 'export_excel']); // export excel
+    Route::get('/export_pdf', [LevelController::class, 'export_pdf']); // export pdf
 
 });
 
@@ -94,6 +96,7 @@
     Route::get('/import', [KategoriController::class, 'import']);
     Route::post('/import_ajax', [KategoriController::class, 'import_ajax']);
     Route::get('/export_excel', [KategoriController::class, 'export_excel']); // export excel
+    Route::get('/export_pdf', [KategoriController::class, 'export_pdf']); // export pdf
    
 
 });
@@ -116,6 +119,7 @@
     Route::get('/import',[SupplierController::class,'import']); // ajax form upload excel
     Route::post('/import_ajax',[SupplierController::class,'import_ajax']); // ajax form import excel    
     Route::get('/export_excel', [SupplierController::class, 'export_excel']); // export excel
+    Route::get('/export_pdf', [SupplierController::class, 'export_pdf']); // export pdf
 
 });
 
@@ -139,8 +143,6 @@
     Route::get('/export_excel', [BarangController::class, 'export_excel']); // export excel
     Route::get('/export_pdf', [BarangController::class, 'export_pdf']); // export pd
 
-
-    
     });
 
 });
