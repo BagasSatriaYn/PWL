5a7c0a75a8d05232a8f07edc117926fd89393e06
BagasSatriaYn <duhjvdedhvhjufahfy@gmail.com>
2025-04-14 19:43:50 +0700

    Memperbaiki Tombol Detail

--- Pos/app/Http/Controllers/KategoriController.php
+++ Pos/app/Http/Controllers/KategoriController.php
@@ -41,8 +41,8 @@ class KategoriController extends Controller
                 // $btn .= '<form class="d-inline-block" method="POST" action="' . url('/kategori/' . $category->kategori_id) . '">'
                 //     . csrf_field() . method_field('DELETE') .
                 //     '<button type="submit" class="btn btn-danger btn-sm" onclick="return confirm(\'Apakah Anda yakin menghapus data ini?\');">Hapus</button></form>';
-                // $btn = '<button onclick="modalAction(\'' . url('/kategori/' . $category->kategori_id . '/show_ajax') . '\')" class="btn btn-info btn-sm">Detail</button> ';
-                $btn = '<button onclick="modalAction(\'' . url('/kategori/' . $category->kategori_id . '/edit_ajax') . '\')" class="btn btn-warning btn-sm">Edit</button> ';
+                $btn = '<button onclick="modalAction(\'' . url('/kategori/' . $category->kategori_id . '/show_ajax') . '\')" class="btn btn-info btn-sm">Detail</button> ';
+                $btn .= '<button onclick="modalAction(\'' . url('/kategori/' . $category->kategori_id . '/edit_ajax') . '\')" class="btn btn-warning btn-sm">Edit</button> ';
                 $btn .= '<button onclick="modalAction(\'' . url('/kategori/' . $category->kategori_id . '/delete_ajax') . '\')" class="btn btn-danger btn-sm">Hapus</button> ';
                 return $btn;
             })
