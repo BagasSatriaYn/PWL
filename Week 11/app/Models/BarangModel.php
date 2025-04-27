b744196a333f6ede7884e06258a02b321a099a4b
BagasSatriaYn <duhjvdedhvhjufahfy@gmail.com>
2025-04-27 16:22:29 +0700

    Tugas Barang

--- Week 11/app/Models/BarangModel.php
+++ Week 11/app/Models/BarangModel.php
@@ -5,20 +5,42 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+ use Tymon\JWTAuth\Contracts\JWTSubject;
+ use Illuminate\Foundation\Auth\User as Authenticatable; // implementasi class Authenticatable
 
-class BarangModel extends Model
+ class BarangModel extends Authenticatable implements JWTSubject
 {
+    public function getJWTIdentifier(){ 
+        return $this->getKey();
+    }
+    
+    public function getJWTCustomClaims(){ return [];
+    }
     use HasFactory;
-    protected $table = 'm_barang';
-    protected $primaryKey = 'barang_id';
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = ['kategori_id', 'barang_kode', 'barang_nama', 'harga_beli', 'harga_jual'];
+ 
+    protected $table = 'm_barang'; // Nama tabel di database
+    protected $primaryKey = 'barang_id'; // Primary Key
+    public $timestamps = true; // Menggunakan timestamps
 
-    public function kategori(): BelongsTo 
+    protected $fillable = [
+        'barang_kode',
+        'barang_nama',
+        'kategori_id',
+        'harga_beli',
+        'harga_jual',
+        'image' // tambah ini
+    ];
+
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($image) => $image ? url('/storage/barang/' . $image) : null
+        );
+    }
+
+    // Relasi ke tabel kategori (m_kategori)
+    public function kategori()
     {
         return $this->belongsTo(KategoriModel::class, 'kategori_id', 'kategori_id');
     }
