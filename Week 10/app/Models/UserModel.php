e28039e71e4e8039a0abf285c6009225ec14d971
BagasSatriaYn <duhjvdedhvhjufahfy@gmail.com>
2025-04-27 13:56:55 +0700

    Praktikum 1

--- Week 10/app/Models/UserModel.php
+++ Week 10/app/Models/UserModel.php
@@ -1,50 +1,33 @@
 <?php
- 
- namespace App\Models;
- 
- use Illuminate\Database\Eloquent\Factories\HasFactory;
- use Illuminate\Database\Eloquent\Model;
- use Illuminate\Database\Eloquent\Relations\BelongsTo;
- use Tymon\JWTAuth\Contracts\JWTSubject;
- use Illuminate\Foundation\Auth\User as Authenticatable;
- 
- class UserModel extends Authenticatable implements JWTSubject
- {
-     public function getJWTIdentifier()
-     {
-         return $this->getKey();
-     }
- 
-     public function getJWTCustomClaims()
-     {
-         return [];
-     }
- 
-     use HasFactory;
-     protected $table = 'm_user';
-     protected $primaryKey = 'user_id';
-     protected $fillable = ['level_id', 'username', 'nama', 'password', 'user_profile_picture'];
- 
-     protected $hidden = ['password'];
-     protected $casts = ['password' => 'hashed'];
- 
-     public function level(): BelongsTo
-     {
-         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
-     }
- 
-     public function getRoleName(): string
-     {
-         return $this->level->level_name;
-     }
- 
-     public function hasRole($role): bool
-     {
-         return $this->level->level_kode == $role;
-     }
 
-     public function getRole() 
-     {
-         return $this->level->level_kode;
-     }
- }
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model; use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+class UserModel extends Authenticatable implements JWTSubject
+{
+
+    public function getJWTIdentifier(){ return $this->getKey();
+        }
+
+    public function getJWTCustomClaims(){ return [];
+        }
+
+    protected $table = 'm_user'; protected $primaryKey = 'user_id';
+
+    protected $fillable = [ 'username', 'nama', 'password', 'level_id', 'image'//tambahan
+];
+
+    public function level()
+        {
+    return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+        }
+    protected function image(): Attribute
+        {
+    return Attribute::make(
+    get: fn ($image) => url('/storage/posts/' . $image),
+ );
+    }
+
+}
