2b5ad1ffedd08857abad7290cd5eeca3cb39d0fb
BagasSatriaYn <duhjvdedhvhjufahfy@gmail.com>
2025-04-27 11:56:16 +0700

    Praktikum 1

--- Week 10/app/Models/UserModel.php
+++ Week 10/app/Models/UserModel.php
@@ -5,12 +5,22 @@
  use Illuminate\Database\Eloquent\Factories\HasFactory;
  use Illuminate\Database\Eloquent\Model;
  use Illuminate\Database\Eloquent\Relations\BelongsTo;
+ use Tymon\JWTAuth\Contracts\JWTSubject;
  use Illuminate\Foundation\Auth\User as Authenticatable;
  
- class UserModel extends Authenticatable
+ class UserModel extends Authenticatable implements JWTSubject
  {
-     use HasFactory;
+     public function getJWTIdentifier()
+     {
+         return $this->getKey();
+     }
  
+     public function getJWTCustomClaims()
+     {
+         return [];
+     }
+ 
+     use HasFactory;
      protected $table = 'm_user';
      protected $primaryKey = 'user_id';
      protected $fillable = ['level_id', 'username', 'nama', 'password', 'user_profile_picture'];
